10d6dbfff5c7b18f71382b9ae31d802e69a5bb6c
Brian Kenji Aguilar <anjacular@gmail.com>
2020-06-28 20:09:07 -0400

    Bird moves up when the user taps the screen

--- core/src/com/briankenjiaguilar/flappybird/FlappyBird.java
+++ core/src/com/briankenjiaguilar/flappybird/FlappyBird.java
@@ -10,20 +10,47 @@ public class FlappyBird extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture backgroundImg;
 	Texture [] flappyBird;
-	int flapState;
+	int flapState, gameState;
+	float birdY, velocity, gravity;
 
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		flapState = 0;
+		gameState = 0;
 		backgroundImg = new Texture("bg.png");
 		flappyBird = new Texture[2];
 		flappyBird[0] = new Texture("bird.png");
 		flappyBird[1] = new Texture("bird2.png");
+		birdY = Gdx.graphics.getHeight()/2 - flappyBird[0].getHeight()/2;
+		velocity = 0;
+		gravity = 2;
 	}
 
 	@Override
 	public void render () {
+
+
+		if (gameState != 0) {
+
+			if (Gdx.input.justTouched()) {
+				velocity = -30;
+			}
+
+
+			// Development purposes
+			if (birdY > 0 || velocity < 0) {
+				// Makes the bird fall faster when it is not tapped
+				velocity += gravity;
+				birdY -= velocity;
+			}
+		} else {
+
+			if (Gdx.input.justTouched()) {
+				gameState = 1;
+			}
+
+		}
 
 		// Renders a different image of the bird based off of the
 		// flap state
@@ -35,19 +62,21 @@ public class FlappyBird extends ApplicationAdapter {
 
 		// Starts rendering sprites
 		batch.begin();
+
 		// Draws the background
 		batch.draw(backgroundImg,0,0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-		// Slows down the render speed for the bird flapping
-		try {
-			Thread.sleep(100);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+
 		// Animates the bird flapping based off of the flap state
-		batch.draw(flappyBird[flapState],Gdx.graphics.getWidth()/2 - flappyBird[flapState].getWidth()/2, Gdx.graphics.getHeight()/2 - flappyBird[flapState].getHeight()/2);
+		batch.draw(
+				flappyBird[flapState],
+				Gdx.graphics.getWidth()/2 - flappyBird[flapState].getWidth()/2,
+				birdY
+		);
+
 		batch.end();
 	}
-	
+
+	public void animateGame() {}
 	@Override
 	public void dispose () {
 		batch.dispose();
